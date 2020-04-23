8c1ace69ad0c97ab206e99c45c0e7b57d2c5ff0d
Martin Hamerník <445720@mail.muni.cz>
2020-04-23 10:01:45 +0200

    Add support for follow up actions to errror and notification service

--- projects/kypo-training-agenda/src/lib/services/client/training-notification.service.ts
+++ projects/kypo-training-agenda/src/lib/services/client/training-notification.service.ts
@@ -1,12 +1,16 @@
 /**
  * Displays notifications from training agenda services and components. Should be overridden by client
  */
+import { Observable } from 'rxjs';
+
 export abstract class TrainingNotificationService {
   /**
    * Emits notification
    * @param type type of notification
    * @param message message of notification
-   * @param duration optional duration of the notification
+   * @param action name of the action button displayed in the notification
+   *  Returns observable.
+   *  Value of the observable is true if the provided action was selected, false otherwise (no reaction or dismissed)
    */
-  abstract emit(type: 'error' | 'warning' | 'info' | 'success', message: string, duration?: number): void;
+  abstract emit(type: 'error' | 'warning' | 'info' | 'success', message: string, action?: string): Observable<boolean>;
 }
--- projects/training-agenda-example-app/src/app/services/client-error-handler.service.ts
+++ projects/training-agenda-example-app/src/app/services/client-error-handler.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ClientErrorHandlerService {
-  emit(err: HttpErrorResponse, operation: string): void {
-    console.log(`${err.message} ${operation}`);
+  emit(err: HttpErrorResponse, operation: string, action: string): void {
+    console.log(`${err.message} ${operation} with action ${action}`);
   }
 }
