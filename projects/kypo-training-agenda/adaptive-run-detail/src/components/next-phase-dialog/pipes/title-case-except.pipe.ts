9155a1625b6d441fc9ff07b66f35f83649d53920
244021 <244021@mail.muni.cz>
2021-04-13 12:55:49 +0200

    Fix lint errors

--- projects/kypo-training-agenda/adaptive-run-detail/src/components/next-phase-dialog/pipes/title-case-except.pipe.ts
+++ projects/kypo-training-agenda/adaptive-run-detail/src/components/next-phase-dialog/pipes/title-case-except.pipe.ts
@@ -7,8 +7,8 @@ export class TitleCaseExceptPipe implements PipeTransform {
   transform(value: string): string {
     if (!value) return value;
 
-    let words: RegExp = /\b(?!of|by|the|a|an|on|in|at|to|into|onto)\w+/g;
-    let newVal = value.replace(words, (match) => {
+    const words = /\b(?!of|by|the|a|an|on|in|at|to|into|onto)\w+/g;
+    const newVal = value.replace(words, (match) => {
       return match.replace(/^\w/, (word) => word.charAt(0).toUpperCase() + word.substr(1).toLowerCase());
     });
     return newVal.charAt(0).toUpperCase() + newVal.substr(1);
