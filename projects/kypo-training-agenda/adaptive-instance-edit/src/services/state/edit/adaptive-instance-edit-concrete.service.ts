0c8886adb2c4d985a81debbc33ebad316161af32
Erik Kočamba <445661@mail.muni.cz>
2021-08-31 13:29:20 +0200

    Fix redirect

--- projects/kypo-training-agenda/adaptive-instance-edit/src/services/state/edit/adaptive-instance-edit-concrete.service.ts
+++ projects/kypo-training-agenda/adaptive-instance-edit/src/services/state/edit/adaptive-instance-edit-concrete.service.ts
@@ -66,7 +66,7 @@ export class AdaptiveInstanceEditConcreteService extends AdaptiveInstanceEditSer
       return this.update();
     } else {
       return this.create().pipe(
-        switchMap((id) => from(this.router.navigate([this.navigator.toTrainingInstanceEdit(id)])))
+        switchMap((id) => from(this.router.navigate([this.navigator.toAdaptiveInstanceEdit(id)])))
       );
     }
   }
