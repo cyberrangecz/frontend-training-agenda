572b30f7761b3c4ff2ff38cefa35fbb0ce7cf923
Igor Ignác <244021@mail.muni.cz>
2022-06-14 12:43:17 +0200

    Fix table reload for training run overview table and training instance summary...

--- projects/kypo-training-agenda/instance-summary/src/components/runs/detail/training-run-info.component.ts
+++ projects/kypo-training-agenda/instance-summary/src/components/runs/detail/training-run-info.component.ts
@@ -2,8 +2,9 @@ import { Component, HostBinding, Input, OnInit } from '@angular/core';
 import { TrainingRun, TrainingRunInfo } from '@muni-kypo-crp/training-model';
 import { TrainingRunConcreteService } from '../../../services/state/runs/training-run-concrete.service';
 import { TrainingRunService } from '../../../services/state/runs/training-run.service';
-import { SentinelTable } from '@sentinel/components/table';
+import { SentinelTable, TableLoadEvent } from '@sentinel/components/table';
 import { TrainingInfoTable } from '../../../model/training-info-table';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'kypo-training-run-info',
@@ -27,5 +28,13 @@ export class TrainingRunInfoComponent implements OnInit {
       },
       () => (this.hasError = true)
     );
+  }
+
+  /**
+   * Gets new data for table
+   * @param loadEvent event emitted by table component to get new data
+   */
+  onLoadEvent(loadEvent: TableLoadEvent): void {
+    this.trainingRunService.getInfo(this.data.id).pipe(take(1)).subscribe();
   }
 }
