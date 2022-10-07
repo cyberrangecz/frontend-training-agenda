83452a692faf10eadb6cae253b41bd78ef4cf740
Igor Ignác <244021@mail.muni.cz>
2022-10-07 13:08:31 +0200

    Resolve "Adress changes in hurdling visualization"

--- projects/kypo-training-agenda/adaptive-instance-edit/src/components/adaptive-instance-edit/adaptive-instance-form-group.ts
+++ projects/kypo-training-agenda/adaptive-instance-edit/src/components/adaptive-instance-edit/adaptive-instance-form-group.ts
@@ -58,7 +58,7 @@ export class AdaptiveInstanceFormGroup {
     trainingInstance.endTime = this.formGroup.get('endTime').value;
     trainingInstance.title = this.formGroup.get('title').value;
     trainingInstance.trainingDefinition = this.formGroup.get('trainingDefinition').value;
-    trainingInstance.accessToken = this.formGroup.get('accessTokenPrefix').value.trim();
+    trainingInstance.accessToken = this.formGroup.get('accessTokenPrefix').value?.trim();
     trainingInstance.localEnvironment = this.formGroup.get('localEnvironment').value;
     trainingInstance.backwardMode = this.formGroup.get('backwardMode').value;
   }
